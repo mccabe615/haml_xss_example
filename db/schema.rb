93c0878c14d1015ce0f60fe8e3084f8d0a53f2c1
Mike McCabe <mike.mccabe@stratumsecurity.com>
2017-01-28 12:06:41 -0500

    updating database

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 
 ActiveRecord::Schema.define(version: 20131220123456) do
 
-  create_table "tasks", force: true do |t|
-    t.string   "title"
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title",      limit: 255
     t.text     "note"
     t.date     "completed"
     t.datetime "created_at"
@@ -22,20 +22,20 @@ ActiveRecord::Schema.define(version: 20131220123456) do
     t.integer  "user_id"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "confirmation_token"
+    t.string   "confirmation_token",     limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
   end
